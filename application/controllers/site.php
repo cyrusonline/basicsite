047eaec0902ae38f0df8ea0d762fe45591485b67
C.K. Chan <chanchikin@ied.edu.hk>
2015-08-18 17:17:29 +0800

    finish

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -25,10 +25,44 @@ class Site extends CI_Controller {
 		$this->load->view("site_footer");
 	}
 	public function contact(){
+		$data["message"]="";
+		
 		$this->load->view("site_header");
 		$this->load->view("site_nav");
-		$this->load->view("content_contact");
+		$this->load->view("content_contact",$data);
 		$this->load->view("site_footer");
+	}
+	
+	public function send_email(){
+		$this->load->library("form_validation");
+		$this->form_validation->set_rules("fullName","Name","required|alpha|xss_clean");
+		$this->form_validation->set_rules("email","Email Address","required|valid_email|xss_clean");
+		$this->form_validation->set_rules("message","Messagesss","required");
+		
+		if($this->form_validation->run()==FALSE){
+			$data['message']="";
+			
+			$this->load->view("site_header");
+			$this->load->view("site_nav");
+			$this->load->view("content_contact",$data);
+			$this->load->view("site_footer");
+		}else {
+			$data["message"]="The email has successfully been sent";
+			$this->load->library("email");
+			$this->email->from(set_value("fullName"), set_value("fullName"));
+			$this->email->to("chanchikin@ied.edu.hk");
+			$this->email->subject("Message from out form");
+			$this->email->message(set_value("message"));
+			$this->email->send();
+			echo $this->email->print_debugger();
+			
+			$this->load->view("site_header");
+			$this->load->view("site_nav");
+			$this->load->view("content_contact",$data);
+			$this->load->view("site_footer");
+		}
+				
+				
 	}
 	
 }
